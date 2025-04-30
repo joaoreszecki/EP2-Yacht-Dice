a1aeda25196937ab5e82d087a38b2a6cddccdfdf
João Reszecki <jreszecki@gmail.com>
2025-04-30 17:36:38 -0300

    Questão 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,13 @@ def rolar_dados(quantidade):
         dados = random.randint(1,6)
         dados_rolados.append(dados)
     return dados_rolados
+
+def guardar_dado(dados_rolados, dados_guardados, indice):
+    dado = dados_rolados[indice]
+    nova_lista = []
+    for i in range(len(dados_rolados)):
+        if i != indice:
+            nova_lista.append(dados_rolados[i])
+    
+    dados_guardados.append(dado)
+    return [nova_lista, dados_guardados]

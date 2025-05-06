909a2732f1be80c4a51d2085e9719c8fb736ea6b
João Reszecki <jreszecki@gmail.com>
2025-05-06 15:07:15 -0300

    Questão 7

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+#Questão 1
 import random
 
 def rolar_dados(quantidade):
@@ -7,6 +8,7 @@ def rolar_dados(quantidade):
         dados_rolados.append(dados)
     return dados_rolados
 
+#Questão 2
 def guardar_dado(dados_rolados, dados_guardados, indice):
     dado = dados_rolados[indice]
     nova_lista = []
@@ -17,6 +19,7 @@ def guardar_dado(dados_rolados, dados_guardados, indice):
     dados_guardados.append(dado)
     return [nova_lista, dados_guardados]
 
+#Questão 3
 def remover_dado(dados_rolados, dados_guardados, indice):
     dado = dados_guardados[indice]
 
@@ -29,6 +32,7 @@ def remover_dado(dados_rolados, dados_guardados, indice):
     
     return [dados_rolados, nova_lista_guardados]
 
+#Questão 4
 def calcula_pontos_regra_simples(lista_face):
     pontuacoes = {}
 
@@ -41,12 +45,14 @@ def calcula_pontos_regra_simples(lista_face):
 
     return pontuacoes
 
+#Questão 5
 def calcula_pontos_soma(lista_face):
     soma = 0
     for valor in lista_face:
         soma += valor
     return soma
 
+#Questão 6
 def calcula_pontos_sequencia_baixa(lista_face):
     unicos = []
     for valor in lista_face:
@@ -66,4 +72,25 @@ def calcula_pontos_sequencia_baixa(lista_face):
                 i += 1
         if i == 4:
             return 15
+    return 0
+
+#Questão 7
+def calcula_pontos_sequencia_alta(lista_face):
+    unicos = []
+
+    for valor in lista_face:
+        if valor not in unicos:
+            unicos.append(valor)
+    validos = [
+        [1,2,3,4,5],
+        [2,3,4,5,6]
+    ]
+
+    for sequencia in validos:
+        i = 0
+        for numero in sequencia:
+            if numero in unicos:
+                i += 1
+        if i == 5:
+            return 30
     return 0
